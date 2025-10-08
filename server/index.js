bf1ad378dab041a40c1a07d0194b09b6dc0123d6
Khazri <121680736+Khazri71@users.noreply.github.com>
2025-10-08 21:56:57 +0200

    deploy

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.use(cors({
     "https://mern-application-web-de-gestion-de-8qm7.onrender.com", 
     "http://localhost:5173"
   ],
+  allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
